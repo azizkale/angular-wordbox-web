72989d9e7c83e7a784f1e049a48c2b7942a6d72d
Mete Demirtas <metedemirtas26@gmail.com>
2020-07-26 00:36:28 +0200

    proxy config multi target-api

--- src/app/vocabulary.service.ts
+++ src/app/vocabulary.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+
+import { catchError, retry } from 'rxjs/operators';
 
 import { Vocabulary } from '../app/models/vocabulary';
 
@@ -16,12 +18,49 @@ export class VocabularyService {
 
   wordsOfSelectedLevel: Vocabulary[];
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
 
   constructor(private httpClient: HttpClient) { }
+
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
 
   getVocabularies(): Observable<Vocabulary[]> {
     return this.httpClient
       .get<Vocabulary[]>(this.vocabularyUrl)
+  }
+
+  getByIDFromUrl() {
+    return this.httpClient.get('/jwbapi/vocabulary/16101');
+  };
+
+  addVocabularyTest(vocabulary: Vocabulary): Observable<Vocabulary> {
+    return this.httpClient.post<Vocabulary>('/jwbapi/add', vocabulary, this.httpOptions);
+  }
+
+  updateVocabularyTest(vocabulary: Vocabulary): Observable<Vocabulary> {
+    return this.httpClient.post<Vocabulary>('/jwbapi/update', vocabulary, this.httpOptions);
+  }
+
+  deleteVocabularyTest(vocabulary: Vocabulary): Observable<Vocabulary> {
+    return this.httpClient.post<Vocabulary>('/jwbapi/delete', vocabulary, this.httpOptions);
   }
 
   getFromDwdd() {
