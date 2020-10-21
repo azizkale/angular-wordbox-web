eb7cdce85653d9c0f836fc9b6bfa254e12a00a2d
azizkale <azizkale@hotmail.com>
2020-10-21 11:18:17 +0200

    commitDeneme

--- src/app/big-dictionary/big-dictionary.component.ts
+++ src/app/big-dictionary/big-dictionary.component.ts
@@ -18,17 +18,17 @@ export class BigDictionaryComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  ShowTheWord(word) {
+  ShowTheWord(word):void {
     this.SentencesFromGlosbe(word);
     // this.GetFromLinguee(word);
   }
 
-  SentencesFromGlosbe(word) {
+  SentencesFromGlosbe(word):void{
     this.vocabularyService.getFromGlosbe(word).subscribe((data) => {
-      var htmlObject = document.createElement('div');
+      const htmlObject = document.createElement('div');
       htmlObject.innerHTML = data;
 
-      //gets related DOM elements from Glosbe
+      // gets related DOM elements from Glosbe
       // this.divID.nativeElement.innerHTML = htmlObject.outerHTML;
 
       const allLiTags = htmlObject
@@ -41,35 +41,38 @@ export class BigDictionaryComponent implements OnInit {
       let index = 0;
 
       for (let i = 0; i < allLiTags.length; i++) {
-        if (i % 3 == 0) liTagsHaveMeaningOfTheWords.push(allLiTags[i]);
+        if (i % 3 === 0){ 
+        liTagsHaveMeaningOfTheWords.push(allLiTags[i]);
       }
 
       liTagsHaveMeaningOfTheWords.forEach((li) => {
         this.meaningsOfTheWord[index] = new meaningsOfTheWord();
 
-        //the Word
+        // the Word
         this.meaningsOfTheWord[index].word = word;
-        //all meanings
+        // all meanings
         this.meaningsOfTheWord[index].meaning = li.getElementsByClassName('text-info')[0][
           'childNodes'
         ][0].textContent;
 
-        //the meanings only have type underneath
+        // the meanings only have type underneath
         if (
           li
             .getElementsByClassName('text-info')
             .item(0)
             .getElementsByClassName('gender-n-phrase')
             .item(0) != null
-        )
+        ){
           this.meaningsOfTheWord[index].type = li
-            .getElementsByClassName('text-info')
-            .item(0)
-            .getElementsByClassName('gender-n-phrase')
-            .item(0)
-            ['childNodes'][0].textContent.replace(/\s+/g, '');
+          .getElementsByClassName('text-info')
+          .item(0)
+          .getElementsByClassName('gender-n-phrase')
+          .item(0)
+          ['childNodes'][0].textContent.replace(/\s+/g, '');
+        }
+          
 
-        //example sentences(german)
+        // example sentences(german)
         if (li.getElementsByClassName('examples').item(0) != null) {
           this.meaningsOfTheWord[index].exampleSentencesInGerman = li
             .getElementsByClassName('examples')
@@ -79,7 +82,7 @@ export class BigDictionaryComponent implements OnInit {
             .getElementsByTagName('div')[0]
             .innerText.trim();
 
-          //example sentences(turkish)
+          // example sentences(turkish)
           this.meaningsOfTheWord[index].exampleSentencesInTurkish = li
             .getElementsByClassName('examples')
             .item(0)
@@ -87,17 +90,17 @@ export class BigDictionaryComponent implements OnInit {
             .getElementsByTagName('div')[3].innerText;
         }
 
-        //removes the words from array, which have no type
-        if (this.meaningsOfTheWord[index].type == undefined) {
+        // removes the words from array, which have no type
+        if (this.meaningsOfTheWord[index].type === undefined) {
           this.meaningsOfTheWord.splice(index, 1);
         } else {
           index++;
         }
       });
 
-      //to make the words with upprecase, which are noun
+      // to make the words with upprecase, which are noun
       this.meaningsOfTheWord.forEach((word) => {
-        if (word.type == '{noun}') {
+        if (word.type === '{noun}') {
           word.word = word.word.charAt(0).toUpperCase() + word.word.slice(1);
         }
       });
