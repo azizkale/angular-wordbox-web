4c7a73eb2f301c8482687d794fc6856bfae34a2b
azizkale <azizkale@hotmail.com>
2020-10-21 15:13:30 +0200

    LearnedWordLintOK

--- src/app/learnedwords/learnedwords.component.ts
+++ src/app/learnedwords/learnedwords.component.ts
@@ -3,7 +3,7 @@ import { VocabularyService } from '../vocabulary.service';
 import { Vocabulary } from '../models/vocabulary';
 
 @Component({
-  selector: 'learnedwords',
+  selector: 'app-learnedwords',
   templateUrl: './learnedwords.component.html',
   styleUrls: ['./learnedwords.component.css'],
 })
@@ -11,34 +11,34 @@ export class LearnedwordsComponent implements OnInit {
   levelVocabularies: Vocabulary[];
   savedVocabularies: Vocabulary[];
 
-  constructor(private vocabularyService: VocabularyService) {}
+  constructor(private vocabularyService: VocabularyService) { }
 
   ngOnInit(): void {
     this.GetLevelWordsFromJSON();
     this.GetSavedWordsFromLocalStorage();
   }
 
-  GetLevelWordsFromJSON() {
+  GetLevelWordsFromJSON(): void {
     this.levelVocabularies = this.vocabularyService.wordsOfSelectedLevel;
   }
 
-  GetSavedWordsFromLocalStorage() {
+  GetSavedWordsFromLocalStorage(): void {
     this.savedVocabularies = new Array<Vocabulary>();
     this.levelVocabularies.map((word: Vocabulary) => {
       this.savedVocabularies.push(JSON.parse(localStorage.getItem(word.id.toString())));
     });
   }
 
-  StarsArray() {
-    let array: Object[] = new Array(5);
+  StarsArray(): Array<object> {
+    const array: object[] = new Array(5);
     array.fill(['assets/images/stargray.png', 'assets/images/staryellow.png']);
     return array;
   }
 
-  StarsCSS() {
-    let _width = window.outerWidth < 400 ? '30%' : '20%';
+  StarsCSS(): object {
+    const Width = window.outerWidth < 400 ? '30%' : '20%';
     return {
-      width: _width,
+      width: Width,
       height: 'auto',
     };
   }
